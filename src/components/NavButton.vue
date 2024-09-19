c61ddcd893414679f9dd0dcc1cbb4b50af1ba9c3
Seunghyo <shyo0218@naver.com>
2024-09-19 15:58:08 +0900

    feat: dummy

--- src/components/NavButton.vue
+++ src/components/NavButton.vue
@@ -1,6 +1,6 @@
 <template>
   <RouterLink :to="link">
-    <div class="nav-container" :style="{background: color }">
+    <div class="nav-container" :style="{ background: props.color || '#FFF' }">
       <span class="nav-container__text" v-html="title"></span>
     </div>
   </RouterLink>
@@ -10,7 +10,7 @@
 import { defineProps } from 'vue';
 import { RouterLink } from 'vue-router';
 
-defineProps({
+const props = defineProps({
   link: {
     type: String,
     required: true,
@@ -29,18 +29,20 @@ defineProps({
 
 <style lang="scss">
 .nav-container {
-  background: #FFF;
+  //background: #FFF;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: auto;
+  height: 100%;
+  min-height: 4rem;
   max-height: 4.0625rem;
   flex-shrink: 0;
   border-radius: 0.1875rem;
   border: 1px solid #B5B5B5;
   padding: 16px;
   box-sizing: border-box;
+  overflow: hidden;
 
   &__text {
   color: #000;
