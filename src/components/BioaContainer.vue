514abe13e4003a9cafa61fe640363294c7a223c1
gitsuhyun <suhyun10201@naver.com>
2024-09-19 14:17:07 +0900

    [Feat] 버튼 컨테이너

---
+++ src/components/BioaContainer.vue
@@ -0,0 +1,42 @@
+<template>
+      <div class="bioa-container" :style="{background: color }">
+        {{ title }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+defineProps({
+  title: {
+    type: String,
+    default: () => null,
+    required: false,
+  },
+  color: {
+    type: String,
+    default: '#FFF',
+  }
+});
+</script>
+
+<style lang="scss">
+.bioa-container {
+  background: #FFF;
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+  width: 7.5rem;  
+  height: 4.0625rem;
+  flex-shrink: 0;
+  border-radius: 0.1875rem;
+  border: 1px solid #B5B5B5;
+  /* text */
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 0.8125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+</style>
